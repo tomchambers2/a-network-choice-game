e19fea48229d28edabc561df5d491abf77cf0146
Tom Chambers <tom.chambers@gmail.com>
2020-03-28 17:25:51 +0000

    demo ready

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef, useCallback } from "react";
 import "./App.css";
 
 import { Graph } from "react-d3-graph";
@@ -6,11 +6,16 @@ import faker from "faker";
 
 const createRandomProperty = () => Math.floor(Math.random() * 10);
 
-const createNpc = () => ({
-  id: faker.name.firstName(),
-  sociability: createRandomProperty(),
-  likability: createRandomProperty()
-});
+const createNpc = () => {
+  const good = Math.random() > 0.5 ? true : false;
+  return {
+    id: faker.name.firstName(),
+    sociability: createRandomProperty(),
+    likability: createRandomProperty(),
+    good,
+    color: good ? "green" : "red"
+  };
+};
 
 const username = "You";
 
@@ -24,44 +29,74 @@ function App() {
     { source: username, target: nodes[1].id },
     { source: username, target: nodes[2].id }
   ]);
+  const [control, setControl] = useState("connect");
+
+  const linksRef = useRef(links);
+  const nodesRef = useRef(nodes);
+
+  useEffect(() => {
+    nodesRef.current = nodes;
+    linksRef.current = links;
+  }, [nodes, links]);
 
   const addLink = (source, target) => {
     const newLink = {
       source: source,
       target: target
     };
-    setlinks(prevLinks => [...prevLinks, newLink]);
+    setlinks(prevLinks => {
+      const newLinks = [...prevLinks, newLink];
+      linksRef.current = newLinks;
+      return newLinks;
+    });
+  };
+
+  const addUserLink = target => {
+    if (
+      linksRef.current.find(
+        ({ source: linkSource, target: linkTarget }) =>
+          (linkSource === username && linkTarget === target) ||
+          (linkTarget === username && linkSource === target)
+      )
+    )
+      return;
+    addLink(username, target);
   };
 
   const globalTurn = newLink => {
     if (newLink) addLink(username, newLink);
-    setNodes(prevNodes => [...prevNodes, createNpc()]);
-    nodes.filter(({ id }) => id !== username).map(npcTurn);
+    if (nodesRef.current.length <= 30)
+      setNodes(prevNodes => {
+        const newNodes = [...prevNodes, createNpc()];
+        nodesRef.current = newNodes;
+        return newNodes;
+      });
+    nodesRef.current.filter(({ id }) => id !== username).map(npcTurn);
   };
+
+  const memoizedGlobalTurn = useCallback(() => globalTurn(), [nodes]);
 
   const npcTurn = node => {
     // link with one other node in the network that is nearby
     // find nearby networks (not implementing yet as nodes don't have locations)
-    console.log(node.id, nodes, links);
-    const acceptableNodes = nodes.filter(
+    const acceptableNodes = nodesRef.current.filter(
       ({ id, likability }) =>
         likability > node.sociability &&
         id !== node.id &&
-        links.findIndex(
+        linksRef.current.findIndex(
           ({ source, target }) =>
             (source === node.id && target === id) ||
             (target === node.id && source === id)
         ) < 0
     );
-    if (!acceptableNodes.length) console.log("no nodes for", node.id);
+
     if (!acceptableNodes.length) return;
     const chosenNode =
       acceptableNodes[Math.floor(Math.random() * acceptableNodes.length)];
-    console.log("add", node.id, "to", chosenNode.id);
     addLink(node.id, chosenNode.id);
   };
 
-  const totalSizeOfNetwork = origin => {
+  const totalSizeOfNetwork = (origin, customFilter) => {
     let processedNodes = [];
 
     const nextLayer = (acc, nodeId) => {
@@ -71,25 +106,38 @@ function App() {
         .filter(id => id !== username)
         .filter(id => processedNodes.indexOf(id) < 0);
 
+      const customFilteredConnections = customFilter
+        ? connectionsOfNode
+            .map(nodeId => nodes.find(({ id }) => id === nodeId))
+            .filter(customFilter)
+            .map(node => node.id)
+        : connectionsOfNode;
+
       processedNodes = [...processedNodes, ...connectionsOfNode];
 
       if (!connectionsOfNode.length) return acc;
       return connectionsOfNode.reduce(
         nextLayer,
-        acc + connectionsOfNode.length
+        acc + customFilteredConnections.length
       );
     };
 
     return nextLayer(0, origin);
   };
 
-  // remove a nodes connections
+  const removeNodeConnections = node => {
+    const newLinks = linksRef.current.filter(
+      ({ source, target }) => !(source === node || target === node)
+    );
+    linksRef.current = newLinks;
+    setlinks(newLinks);
+  };
 
   useEffect(() => {
-    const interval = setInterval(globalTurn, 10000);
+    const interval = setInterval(memoizedGlobalTurn, 10000);
 
-    return () => clearInterval(interval.current);
-  }, [globalTurn]);
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <div className="App">
@@ -102,6 +150,28 @@ function App() {
         }
       </p>
       <p>Total network size: {totalSizeOfNetwork(username)}</p>
+      <p>
+        Good nodes in your network:{" "}
+        {totalSizeOfNetwork(username, node => node.good)}
+      </p>
+      <p>
+        Evil nodes in your network:{" "}
+        {totalSizeOfNetwork(username, node => !node.good)}
+      </p>
+      <div className="Controls">
+        <div
+          className={`Connect ${control === "connect" && "active"}`}
+          onClick={() => setControl("connect")}
+        >
+          CONNECT
+        </div>{" "}
+        <div
+          className={`Destroy ${control === "destroy" && "active"}`}
+          onClick={() => setControl("destroy")}
+        >
+          DESTROY
+        </div>
+      </div>
       <Graph
         id="graph-id"
         config={{
@@ -110,7 +180,10 @@ function App() {
           }
         }}
         data={{ nodes, links }}
-        onClickNode={globalTurn}
+        // onClickNode={memoizedGlobalTurn}
+        onClickNode={
+          control === "connect" ? addUserLink : removeNodeConnections
+        }
       ></Graph>
     </div>
   );
